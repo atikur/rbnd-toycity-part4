4fdb157050ab8491c58fa51b29daeeebb7665e54
Atikur Rahman <atik.bd08@gmail.com>
2016-02-24 11:46:14 +0600

    add create method

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -3,5 +3,20 @@ require_relative 'errors'
 require 'csv'
 
 class Udacidata
-  # Your code goes here!
+  def self.create(options={})
+    file = File.dirname(__FILE__) + "/../data/data.csv"
+    existing_contents = CSV.read(file)
+    
+    obj = self.new(options)
+
+    if (obj.is_a? Product)
+      new_record = [obj.id, obj.brand, obj.name, obj.price]
+      if !existing_contents.include?(new_record)
+      	CSV.open(file, "ab") do |csv|
+      	  csv << new_record
+      	end
+      end
+    end
+    obj
+  end
 end
