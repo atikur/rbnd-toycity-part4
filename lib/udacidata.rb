ca422ee95cbe4a7d7398961f70baa072290a6a8a
Atikur Rahman <atik.bd08@gmail.com>
2016-02-25 16:58:22 +0600

    test app.rb script

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -36,7 +36,7 @@ class Udacidata
     obj = self.new(options)
 
     if (obj.is_a? Product)
-      new_record = [obj.id, obj.brand, obj.name, obj.price]
+      new_record = [obj.id.to_s, obj.brand, obj.name, obj.price]
       if !existing_contents.include?(new_record)
       	CSV.open(file, "ab") do |csv|
       	  csv << new_record
